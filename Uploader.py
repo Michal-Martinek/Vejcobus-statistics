17a5f23982601cd9baeca6b87c67494b56998493
Michal Martínek <michal7952@seznam.cz>
2024-01-21 20:22:06 +0100

    Uploader maximizing server console

--- Uploader.py
+++ Uploader.py
@@ -1,9 +1,12 @@
 import os, sys
 import json
 import paramiko
+import win32gui, win32con
 
 os.chdir(os.path.dirname(__file__))
 REMOTE_PATH = "/home/azureuser/vejcobus-statistics"
+
+TERMINAL_WINDOW_NAME = 'Michal-Server'
 
 def getServerCredentials():
 	with open('server_credentials.json') as f:
@@ -16,18 +19,37 @@ def openSFTP():
 	sftp = ssh.open_sftp()
 	return sftp
 def uploadFile(sftp, uploadingPath):
-	remotePath = f'{REMOTE_PATH}/{os.path.basename(uploadingPath)}'
-	params = sftp.put(uploadingPath, remotePath, callback=None)
+	remoteFilename = os.path.basename(uploadingPath)
+	if remoteFilename not in sftp.listdir(REMOTE_PATH): print(f"INFO: created new file '{remoteFilename}'")
+	params = sftp.put(uploadingPath, f'{REMOTE_PATH}/{remoteFilename}', callback=None)
+	print(f"Uploaded '{remoteFilename}':", params)
 	sftp.close()
 def getUploadingPath():
 	assert len(sys.argv) > 1, 'Path argument expected'
 	path = sys.argv[-1]
 	assert os.path.exists(path), 'Invalid path argmuent'
 	return os.path.abspath(path)
+# maximize terminal ------------------------
+def getTerminalWindow():
+	windows = {}
+	def winEnumHandler(hwnd, ctx):
+		name = win32gui.GetWindowText(hwnd)
+		windows[name] = hwnd
+	win32gui.EnumWindows(winEnumHandler, None)
+	if TERMINAL_WINDOW_NAME not in windows:
+		print(f'ERROR: Terminal window not found')
+		return None
+	return windows[TERMINAL_WINDOW_NAME]
+def maximizeTerminal():
+	if (hwnd := getTerminalWindow()) is None: return
+	win32gui.ShowWindow(hwnd, win32con.SW_SHOWMAXIMIZED)
+	win32gui.SetForegroundWindow(hwnd)
 def main():
+	print()
 	uploadingPath = getUploadingPath()
 	sftp = openSFTP()
 	uploadFile(sftp, uploadingPath)
+	maximizeTerminal()
 
 if __name__ == '__main__':
 	main()
